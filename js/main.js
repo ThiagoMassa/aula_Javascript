3f08d1ff1fd1c17706bccc501bba16da4c65a3a6
ThiagoMassa <thiagomassa6@gmail.com>
2021-02-18 19:28:06 -0300

    Adiciona arquivos da aula

---
+++ js/main.js
@@ -0,0 +1,134 @@
+/*
+function functionChange(elemento) {
+    console.log(elemento.value);
+}
+*/
+
+/*
+function load() {
+    alert("Pagina carregada");
+}
+*/
+
+/*
+function trocar(elemento) {
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento) {
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+*/
+
+/*
+function redirecionar() {
+    window.open("https://github.com/ThiagoMassa");  // ele vai abrir em uma nova aba 
+    window.location.href = "https://github.com/ThiagoMassa"   // ele vai abrir na mesma pagina
+}
+
+ function clicou() {
+   // document.getElementById("agradecimento");
+     document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar<b>";
+    //console.log(document.getElementById("agradecimento"));
+   // alert("Obrigado por clicar");
+}
+
+function trocar() {
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    // alert("Trocar texto");
+}
+
+function voltar() {
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+*/
+
+/*
+function validaIdade(idade) {
+    var validar = true;
+    if (idade >= 18){  
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qqual sua Idade?");
+console.log(validaIdade(idade));
+*/
+
+/*
+function soma(n1, n2) {
+    return n1 + n2;
+};
+
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+// var d = new Date();
+// alert(d); // apresenta Data completa dd/mm/aaaa e horario com fuso horario
+// alert(d.getMonth()+1); // aparece so o mes em N° || é preciso por o + 1 pois ele conta 0 primeiro como 0
+// alert(d.getMinutes()); // mostra o minuto em que estamos
+// alert(d.getHours()); // mostra a hora em que estamos
+
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    console.log(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;  // Quando se coloca ++ é ele mais 1
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade"); // abre uma aba para coletar informaçao
+// var idade = 18;
+if (idade >= 18){
+    alert("Maior de Idade");
+}else{
+    alert("Menor de Idade");
+}
+*/
+
+// var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+// console.log(frutas)
+// console.log(frutas[1].nome)
+// console.log(fruta);
+// console.log(fruta.nome);
+// console.log(fruta.cor);
+// var lista = ["maça", "pera", "laranja"];
+// lista.push("uva"); // adiciona 
+// lista.pop(); // retira o ultimo elemento "laranja"
+// console.log(lista.reverse()); // ele trouxe a "Uva"(como ultimo elemento) para primeira posiçao
+// console.log(lista.length); // mostra o tamanho da minha lista (quantidade de elementos)
+// console.log(lista[0]);
+// console.log(lista.toString());
+// console.log(lista.join(" - ")); // muda como elas sao separadas
+ 
+
+// var nome = "Thiago Massa";
+// var n1 = 5;
+// var in2 = 3;
+// var frase = "Japao é o melhor time do mundo";
+// alert("Seja Bem vindo!");
+// alert(n1 + n2);
+// console.log(nome);
+// console.log(n1 + n2);
+// console.log(frase.replace("Japao", "Brasil"));
+// console.log(frase.toUpperCase()); // deixa tudo em CAPSLCOK
+// console.log(frase.toLowerCase()); // deixa tudo em minuscula
+// alert(frase.replace("Japao", "Brasil"));
